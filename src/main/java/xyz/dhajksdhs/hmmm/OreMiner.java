8d6240dc6929d5b6ae14f0120d71cac8b94e575a
Qiu Tian <qiu_tian@students.edu.sg>
2025-11-18 13:12:15 +0800

    Removed unused files Respects pickaxe fortune and silk touch Added Command to enable/disable mod for player only.

--- src/main/java/xyz/dhajksdhs/hmmm/OreMiner.java
+++ src/main/java/xyz/dhajksdhs/hmmm/OreMiner.java
@@ -1,16 +1,23 @@
 package xyz.dhajksdhs.hmmm;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.message.MessageType;
 import net.minecraft.server.world.ServerWorld;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.particle.ParticleTypes;
-import java.util.HashSet;
-import java.util.Set;
+
+import java.awt.*;
+import java.util.*;
+import net.minecraft.server.command.CommandManager;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.minecraft.particle.BlockStateParticleEffect;
@@ -23,7 +30,7 @@ public class OreMiner implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+    public static final Map<UUID,Boolean> enabled = new HashMap<>();
 	@Override
 	public void onInitialize() {
         final Set<Block> ores = Set.of(
@@ -46,32 +53,43 @@ public class OreMiner implements ModInitializer {
         );
 		LOGGER.info("Hello Fabric world!");
         PlayerBlockBreakEvents.BEFORE.register((world,player,pos,state,blockEntity)->{
+            if (!enabled.getOrDefault(player.getUuid(),true))return true;
             if (!(world instanceof ServerWorld serverWorld)) return true;
             ItemStack tool = player.getMainHandStack();
             if (!tool.isSuitableFor(state))return true; //check if player is menance
             Block block = state.getBlock();
             if (ores.contains(block)) {
-                helper(serverWorld,block,pos,new HashSet<>());
+                helper(serverWorld,block,pos,new HashSet<>(),player);
                 return true;
             }
             return true;
         });
+        CommandRegistrationCallback.EVENT.register(((d, commandRegistryAccess, registrationEnvironment) ->{
+            d.register(CommandManager.literal("oreminer").executes(con -> {
+                PlayerEntity player =  con.getSource().getPlayer();
+                if (player == null)return 0;
+                boolean playerstate = enabled.getOrDefault(player.getUuid(),true);
+                enabled.put(player.getUuid(),!playerstate);// flip preference
+                player.sendMessage(Text.literal(String.format("Ore Miner is %s.",playerstate?"§Enabled":"§Disabled")), false);
+                return 1;
+            }));
+        } ));
 	}
-    public void helper(ServerWorld world, Block Ore,BlockPos pos, Set<BlockPos> visited){
+    public void helper(ServerWorld world, Block Ore, BlockPos pos, Set<BlockPos> visited, PlayerEntity player) {
         if (visited.contains(pos))return;
         visited.add(pos);
         var state = world.getBlockState(pos);
         Block target = world.getBlockState(pos).getBlock();
         if (!target.equals(Ore))return; // check if ore is same as ore mined
         world.setBlockState(pos, Blocks.AIR.getDefaultState());
-        world.spawnEntity(new ItemEntity(world,pos.getX(),pos.getY(),pos.getZ(),new ItemStack(target.asItem()))); //drop item at desired ore location
+        Block.dropStacks(state,world,pos,null,player,player.getMainHandStack());
         world.spawnParticles(new BlockStateParticleEffect(ParticleTypes.BLOCK, state), pos.getX()+0.5,pos.getY()+0.5,pos.getZ()+0.5,20,0.25,0.25,0.25,0.5); //spawn breaking particles
         //recur
-        helper(world,target,pos.up(),visited);
-        helper(world,target,pos.down(),visited);
-        helper(world,target,pos.north(),visited);
-        helper(world,target,pos.south(),visited);
-        helper(world,target,pos.east(),visited);
-        helper(world,target,pos.west(),visited);
+        helper(world,target,pos.up(),visited, player);
+        helper(world,target,pos.down(),visited,player);
+        helper(world,target,pos.north(),visited,player);
+        helper(world,target,pos.south(),visited,player);
+        helper(world,target,pos.east(),visited,player);
+        helper(world,target,pos.west(),visited,player);
     }
 }
